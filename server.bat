cb6c61d6dd01b5623fd2befa81e7a4e8c56ef276
desulaid <desulaid@yandex.ru>
2022-06-24 03:10:55 +0300

    cleanup code

--- server.bat
+++ server.bat
@@ -1,4 +1,4 @@
 @echo off
 set port=8080
 explorer http://localhost:%port%
-npm start %port%
+node ./app.js %port%
---
+++ src/windows.js
@@ -0,0 +1,32 @@
+const
+    iconv = require('iconv-lite'),
+    { exec } = require('child_process')
+
+// Консольная команда net uset
+const exec_netUser = (callback) => {
+    exec('net user', { encoding: "buffer" },
+        (error, stdout, stderr) => {
+            if (error) {
+                callback(stderr, null)
+            } else {
+                callback(null, iconv.decode(stdout, 'CP866'))
+            }
+        }
+    );
+}
+
+// Создает из результата команды exec_netUser массив
+// со списком пользователей
+const usersTable = (callback) => (exec_netUser((err, res) => {
+    if (err) {
+        console.log(`Error: ${err}`)
+    }
+    else {
+        callback(res
+            .slice(res.lastIndexOf('-') + 1)
+            .match(/([A-Za-zА-Яа-я0-9_]+)/g)
+        )
+    }
+}))
+
+module.exports = usersTable
